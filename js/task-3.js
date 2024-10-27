0f43ea4d6effec5210ba363f8aacc9231c7c89f8
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2024-10-27 19:02:05 +0200

    end

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,31 @@
+class StringBuilder {
+    #valu
+    constructor(initialValue) {
+        this.#valu = initialValue;
+    }
 
+    getValue() {
+        return(this.#valu);
+    }
+
+    padEnd(str) {
+        this.#valu += str;
+    }
+
+    padStart(str) {
+       this.#valu = str + this.#valu; 
+    }
+
+    padBoth(str) {
+        this.#valu = str + this.#valu + str;
+    }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
